9b4397e464ba25886c9d1e950e214d03c39d0042
Long Hoang <longhoang@ucsb.edu>
2021-02-08 20:45:12 -0800

    1/2

--- pi_mc_omp.c
+++ pi_mc_omp.c
@@ -8,6 +8,7 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* Global variables */
 long long int number_in_circle = 0;
@@ -26,17 +27,21 @@ void parallel_loop(int threadcnt) {
   long long int toss;
   long long int sum = 0;
   double x, y, distance_squared;
+#pragma omp parallel private(toss, x, y, distance_squared) num_threads(threadcnt)
+  {
   // Set this to thread id when use OpenMP.
-  int tid = 0;
+   int tid = (int)time(NULL) ^ omp_get_thread_num();
 
-  for (toss = 0; toss < number_of_tosses; toss++) {
+   #pragma omp parallel for reduction(+:sum)
+   for (toss = 0; toss < number_of_tosses; toss++) {
     x = 2LL * rand_r(&tid) / ((double)RAND_MAX) - 1.0;
     y = 2LL * rand_r(&tid) / ((double)RAND_MAX) - 1.0;
-    distance_squared = x * x + y * y;
+    distance_squared = (x * x) + (y * y);
     if (distance_squared <= 1) {
       sum++;
     }
-  }
+   }
+  } 
 
   number_in_circle = sum;
 }
@@ -55,6 +60,8 @@ double parallel_pi(long long int no_tosses, int threadcnt) {
   number_in_circle = 0;
 
   parallel_loop(threadcnt);
-  pi_estimate = 4.0 * number_in_circle / ((double)number_of_tosses);
+
+  printf("Total num in circle: %d\n", number_in_circle);
+  pi_estimate = 4.0 * number_in_circle / (double)number_of_tosses;
   return pi_estimate;
 }
